406963dcf16ad99c76651a11f550f1d0e1ba2164
Mark Buttig <mbuttig@de.pilz.local>
2018-10-30 13:51:12 +0100

    Commenting methods

--- src/smartsensor_barrel.cpp
+++ src/smartsensor_barrel.cpp
@@ -4,6 +4,9 @@
 /**
  * @brief Construct a new smartsensor barrel::smartsensor barrel object
  * 
+ * @param ntp 
+ * @param mqtt 
+ * @param syslog 
  */
 smartsensor_barrel::smartsensor_barrel(smartsensor_ntp *ntp, smartsensor_pubsubclient *mqtt, smartsensor_syslog *syslog)
 : NewPing(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE_CM), ntp(ntp), mqtt(mqtt), syslog(syslog)
@@ -19,7 +22,12 @@ smartsensor_barrel::~smartsensor_barrel()
 {
 }
 
-
+/**
+ * @brief Start meassure
+ * 
+ * @return true 
+ * @return false 
+ */
 bool smartsensor_barrel::do_measure(void)
 {
     bool rc = false;
@@ -38,7 +46,12 @@ bool smartsensor_barrel::do_measure(void)
     return rc;
 }
 
-
+/**
+ * @brief Publish fill level
+ * 
+ * @return true 
+ * @return false 
+ */
 bool smartsensor_barrel::do_publish(void)
 {
     bool rc = true;
@@ -52,7 +65,7 @@ bool smartsensor_barrel::do_publish(void)
 }
 
 /**
- * @brief 
+ * @brief Return fill level struct
  * 
  * @return sFillLevel 
  */
@@ -61,24 +74,42 @@ sFillLevel smartsensor_barrel::get_fill_level(void)
     return fill_level;
 }
 
-
+/**
+ * @brief Return fill level cm
+ * 
+ * @return unsigned long 
+ */
 unsigned long smartsensor_barrel::get_fill_level_cm(void)
 {
     return fill_level.cm;
 }
 
+/**
+ * @brief Return fill level in percent
+ * 
+ * @return float 
+ */
 float smartsensor_barrel::get_fill_level_percent(void)
 {
     return fill_level.percent;
 }
 
+/**
+ * @brief Return timestamp as local date time
+ * 
+ * @return std::string 
+ */
 std::string smartsensor_barrel::get_local_datetime(void)
 {
     return ntp->get_local_datetime(fill_level.utc);
 }
 
-
-        
+/**
+ * @brief Publish fill level as MQTT
+ * 
+ * @return true 
+ * @return false 
+ */
 bool smartsensor_barrel::do_publish_mqtt(void)
 {
     bool rc = true;
@@ -92,6 +123,12 @@ bool smartsensor_barrel::do_publish_mqtt(void)
     return rc;
 }
 
+/**
+ * @brief Publish fill level to syslog
+ * 
+ * @return true 
+ * @return false 
+ */
 bool smartsensor_barrel::do_publish_syslog(void)
 {
     bool rc;
@@ -103,6 +140,13 @@ bool smartsensor_barrel::do_publish_syslog(void)
 
     return rc;
 }
+
+/**
+ * @brief Publish fill level to serial
+ * 
+ * @return true 
+ * @return false 
+ */
 bool smartsensor_barrel::do_publish_serial(void)
 {
     bool rc = false;
