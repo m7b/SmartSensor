b90236672308f5a1c398668f8ed58343f81d7303
Mark Buttig <mbuttig@gmx.net>
2021-06-08 16:02:37 +0200

    optimized script

--- conf/job_copy_certs_mosquitto.sh
+++ conf/job_copy_certs_mosquitto.sh
@@ -1,12 +1,32 @@
 #!/bin/bash
 
-# Copy certificates to mosquitto working directory
-cp /usr/local/etc/certificate/SynologyDrive/SynologyDrive/cert.pem /volume1/@appstore/mosquitto/var
-cp /usr/local/etc/certificate/SynologyDrive/SynologyDrive/chain.pem /volume1/@appstore/mosquitto/var
-cp /usr/local/etc/certificate/SynologyDrive/SynologyDrive/privkey.pem /volume1/@appstore/mosquitto/var
+CERT_SRC=/usr/local/etc/certificate/SynologyDrive/SynologyDrive/cert.pem
+CERT_DST=/volume1/@appstore/mosquitto/var/cert.pem
 
-# Change ownership
-chown sc-mosquitto:mosquitto /volume1/@appstore/mosquitto/var/*.pem
+CHAIN_SRC=/usr/local/etc/certificate/SynologyDrive/SynologyDrive/chain.pem
+CHAIN_DST=/volume1/@appstore/mosquitto/var/chain.pem
 
-# Change mod
-chmod 0644 /volume1/@appstore/mosquitto/var/*.pem
+PRIVKEY_SRC=/usr/local/etc/certificate/SynologyDrive/SynologyDrive/privkey.pem
+PRIVKEY_DST=/volume1/@appstore/mosquitto/var/privkey.pem
+
+# Check certificates changed
+if ! cmp $CERT_SRC $CERT_DST > /dev/null 2>&1
+then
+    # Certificates are different.
+    # Copy certificates to mosquitto working directory
+    cp $CERT_SRC $CERT_DST
+    cp $CHAIN_SRC $CHAIN_DST
+    cp $PRIVKEY_SRC $PRIVKEY_DST
+
+    # Change ownership
+    chown sc-mosquitto:mosquitto $CERT_DST
+    chown sc-mosquitto:mosquitto $CHAIN_DST
+    chown sc-mosquitto:mosquitto $PRIVKEY_DST
+
+    # Change mod
+    chmod 0644 $CERT_DST
+    chmod 0644 $CHAIN_DST
+    chmod 0644 $PRIVKEY_DST
+else
+    # Certificates not changed
+fi
